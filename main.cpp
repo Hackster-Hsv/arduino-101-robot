770c08cbf08e406ce112003e6087c605a2e293b6
Noah Huber-Feely <nhuberfeely@gmail.com>
2016-12-12 15:26:35 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // Initialize robot
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  // Accept and actuate remote commands
+}
