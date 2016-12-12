7f83e434a1f0ee0112f5a066bb3e37956cf11048
Noah Huber-Feely <nhuberfeely@gmail.com>
2016-12-12 15:33:55 -0600

    Add BLE support for Blynk app

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
-// the setup function runs once when you press reset or power the board
+#include <BlynkSimpleCurieBLE.h>
+#include <CurieBLE.h>
+
+char auth[] = "-------Auth Token From BlynkApp-------";
+
+BLEPeripheral  blePeripheral;
+
 void setup() {
-  // Initialize robot
+  Serial.begin(1200);
+  delay(1000);
+
+  blePeripheral.setLocalName("Arduino-Rover");
+  blePeripheral.setDeviceName("Arduino-Rover");
+  blePeripheral.setAppearance(384);
+
+  Blynk.begin(auth, blePeripheral);
+
+  blePeripheral.begin();
+
+  Serial.println("Waiting for connections...");
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  // Accept and actuate remote commands
+  Blynk.run();
+  blePeripheral.poll();
 }
